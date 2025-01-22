c94a6880d2812c9ec1eaa75755aa29de68549cc0
rqp <59442213+noobball111@users.noreply.github.com>
2025-01-22 16:31:27 +0700

    Update Sled.lua

--- Sled.lua
+++ Sled.lua
@@ -15,33 +15,33 @@ local current = {}
 
 for _,spawner: Part in ipairs(Spawners:GetChildren()) do
 	local number = string.match(spawner.Name, "%d+")
-	
+
 	current[spawner.Name] = os.clock()
 	spawner.ProximityPrompt.Triggered:Connect(function(whoTriggered)
 		local elapsed = os.clock() - current[spawner.Name]
 		if elapsed < cooldown then return end
 		current[spawner.Name] = os.clock()
-		
+
 		local Character = whoTriggered.Character
 		if Character == nil or Character.Parent == nil then return end
 
 		local Humanoid:Humanoid = Character:FindFirstChild("Humanoid")
 		if Humanoid == nil or Humanoid.Parent == nil then return end
-		
+
 		local HumanoidRootPart:Part = Character:FindFirstChild("HumanoidRootPart")
 		if HumanoidRootPart == nil or HumanoidRootPart.Parent == nil then return end
-				
+
 		local ClonedSled:MeshPart = Sled:Clone()
 		ClonedSled.Position = SpawnPoints["SpawnSledPart"..number].Position
 		ClonedSled.Seat.Position = ClonedSled.Position
 		ClonedSled.Parent = trashbin
 		local seat: Seat = ClonedSled.Seat
 		--repeat task.wait() until ClonedSled.Parent == trashbin		
-		local Velocity = ClonedSled.Seat.CFrame.LookVector * Vector3.new(1,0,1) * 30
+		local Velocity = ClonedSled.Seat.CFrame.LookVector * Vector3.new(1,0,1) * 40
 		SledEvent:FireClient(whoTriggered, ClonedSled, Velocity, Humanoid)
-		
+
 		--seat:Sit(Humanoid)
-		
+
 		task.defer(function()
 			task.wait(60*5)
 			if ClonedSled == nil then return end
@@ -52,10 +52,8 @@ end
 
 for _,despawn: Part in ipairs(Despawns:GetChildren()) do
 	despawn.Touched:Connect(function(part)
-		local Sled = part:FindFirstAncestor("Sled")
+		if part.Name ~= "Sled" then return end
 		
-		if not Sled then return end
-		
-		Sled:Destroy()
+		part:Destroy()
 	end)
 end
