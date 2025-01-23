19387b90d1c6395169f5fed91cd8ab81bff1cd7b
rqp <59442213+noobball111@users.noreply.github.com>
2025-01-23 12:34:58 +0700

    Update Leaderboard.lua

--- Leaderboard.lua
+++ Leaderboard.lua
@@ -10,7 +10,7 @@ local function GetTop10()
 	local pageSize = 60
 	local pages = pointsStore:GetSortedAsync(isAscending, pageSize)
 	local topTen = pages:GetCurrentPage()
-		
+
 	return topTen
 end
 
@@ -19,24 +19,30 @@ local COOLDOWN = 60
 local cooldown = 0
 local current = os.clock()
 
+local function update()
+	local topTen = GetTop10()
+	for rank,data in GetTop10() do
+		local userID = data.key
+		local value = data.value
+
+		local playerFolder = Folders[rank]
+
+		local success, username = pcall(Players.GetNameFromUserIdAsync, Players, userID)
+		if success then username = username else username = "USER NOT FOUND" end
+
+		playerFolder.Profile.SurfaceGui.ImageLabel.Image = Players:GetUserThumbnailAsync(userID, Enum.ThumbnailType.HeadShot, Enum.ThumbnailSize.Size420x420)
+		playerFolder.Username.SurfaceGui.TextLabel.Text = username
+		playerFolder.WinAmount.SurfaceGui.TextLabel.Text = value
+	end
+end
+
 RunService.Heartbeat:Connect(function()
 	local elapsed = os.clock() - current
 	if elapsed < cooldown then return end
 	current = os.clock()
 	if cooldown ~= COOLDOWN then cooldown = COOLDOWN end
-	
-	local topTen = GetTop10()
-	for rank,data in GetTop10() do
-		local userID = data.key
-		local value = data.value
-		
-		local playerFolder = Folders[rank]
-		
-		local success, username = pcall(Players.GetNameFromUserIdAsync, Players, userID)
-		if success then username = username else username = "USER NOT FOUND" end
-		
-		playerFolder.Profile.SurfaceGui.ImageLabel.Image = Players:GetUserThumbnailAsync(userID, Enum.ThumbnailType.HeadShot, Enum.ThumbnailSize.Size420x420)
-		playerFolder.Username.SurfaceGui.TextLabel.Text = username
-		playerFolder.WinAmount.SurfaceGui.TextLabel.Text = value
-	end
+
+	update()
 end)
+
+update()
